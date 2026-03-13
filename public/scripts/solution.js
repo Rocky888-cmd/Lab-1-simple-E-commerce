bbe4487f7cbbafe3bfb1379778f00d11bcb4c10f
unknown <rockymalobago080@gmail.com>
2026-03-13 16:42:42 +0800

    add generateIDs

--- public/scripts/solution.js
+++ public/scripts/solution.js
@@ -22,3 +22,22 @@ function checkVariable(input) {
             return "unknown type";
     }
 }
+
+function generateIDs(count) {
+
+    const ids = [0,1,2,3,4,5,6,7];
+    
+
+    for (let i = 0; i < ids.length -1 ; i++) {
+        const element = ids[i];
+
+        if(element===5){
+            continue
+        }      
+        const count = ids[i];
+        return count;
+    }
+    
+}
+
+console.log(generateIDs(7));

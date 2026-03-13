260ba3d76be349d3e245e596c1075d65b10ad20c
Rocky Malobago <rockymalobago080@gmail.com>
2026-03-13 18:58:31 +0800

    test

--- public/scripts/solution.js
+++ public/scripts/solution.js
@@ -2,7 +2,7 @@ function checkVariable(input) {
     switch (typeof input) {
         case "string":
             return "string";
-
+        
         case "number":
             return "number";
 
@@ -22,40 +22,10 @@ function checkVariable(input) {
             return "unknown type";
     }
 }
-// const readline = require("readline");
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// rl.question("Enter any datatype: ", function(input) {
-//   console.log(checkVariable(input))
-//   rl.close();
-// });
-
-const prompt = require("prompt-sync")();
-
-let name = prompt("Enter any datatype:");
-if (!isNaN(name)) {
-    name = Number(name);
-
-}
-if (name == "true") {
-    name = name == "true"
-
-
-} else if (name == "false") {
-    name = name == "false"
-}
-
-console.log(checkVariable(name));
-
-
 
 function generateIDs(count) {
 
-    const ids = [0, 1, 2, 3, 4, 5, 6, 7];
+    const ids = [0,1,2,3,4,5,6,7];
     const result = [];
 
     for (let i = 0; i <= count; i++) {
@@ -74,29 +44,17 @@ function generateIDs(count) {
 console.log(generateIDs(7));
 
 
-// try
-function calculateTotal(...numbers) {
-    try {
-        if (!numbers.every(num => typeof num === "number")) {
-            throw new TypeError("Invalid input: All arguments must be numbers");
-        }
 
 
-        return numbers.reduce((sum, num) => sum + num, 0);
-    } catch (error) {
-        console.log(error.message);
-    }
-    // if (!numbers.every(num => typeof num === "number")) {
-    //     throw new TypeError("Invalid input: All arguments must be numbers");
-    // }
-    // return numbers.reduce((sum, num) => sum + num, 0);
-}
 
-console.log("Sum:", calculateTotal(5, 10, 15));
-console.log("Sum:", calculateTotal("1", "7", "3", "4"));
-// try {
-//     console.log("Sum:", calculateTotal(5, 10, 15));
-//     console.log("Sum:", calculateTotal("1", "7", "3", "4"));
-// } catch (error) {
-//     console.log(error.message);
-// }
+
+
+
+
+
+
+
+
+
+
+const players = [{"name":"jona"},{"score":10}]
